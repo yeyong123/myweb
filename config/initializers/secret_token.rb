fca3af8a3c235404ca58cf798d3cdcd915b2941a
yeyong14 <yeyong14@gmail.com>
2013-04-29 20:48:59 +0800

    完成组件安装

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myweb::Application.config.secret_token = 'd5fe0ed4480b96c72639333e72c5e8324c1bb616838a3d9dc072222c560f9ab7a3af0ef7f63f69748d17858e96a05511c4123e54f71d46439db8913cc083d501'
