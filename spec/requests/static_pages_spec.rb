002f73596852f65b2bc1dce5eb949d99e44ff381
yeyong14 <yeyong14@gmail.com>
2013-05-01 12:48:11 +0800

    搭建社交网络的布局，完成页面间的链接

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,31 +1,33 @@
 require 'spec_helper'
-
 describe "Static pages" do
+  subject {page}
+  
   describe "Home page" do
-    it "should have the content 'My Web'" do
-      visit '/static_pages/home'
-      page.should have_content('My Web')
-    end
-  end
 
-  describe "Help page" do
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
-    end
+    before {visit root_path }
+
+    it { should have_selector('h1', text: 'Twitter?') }
+    it { should have_selector('title', text: full_title('') )}
+    it { should_not  have_selector 'title', text: '| 主页'}
   end
 
   describe "About Us" do
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_content('About Us')
-    end
+    before { visit about_path }
+
+    it { should have_selector('h1', text: 'About Us')}
+    it { should have_selector('title', text: full_title('About Us'))}
   end
 
   describe "Contact" do
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_content('Contact')
-    end
+    before { visit contact_path }
+    it { should have_selector('h1', text: 'Contact')}
+    it { should have_selector('title', text: full_title('Contact'))}
+  end
+
+  describe "Help" do
+    before { visit help_path }
+    it { should have_selector('h1', text: 'Help')}
+    it { should have_selector('title', text: full_title('Help'))}
   end
 end
+
